322c75a6857d838ea0ce4ee7899aae047d5efd92
Roman Kapshuk <roman.kapshuk@agilie.com>
2017-05-24 14:54:46 +0300

    Delete unused classes from sample

--- app/src/main/java/test/alexzander/swipetodelete/ItemSwipeListener.kt
+++
@@ -1,9 +0,0 @@
-package test.alexzander.swipetodelete
-
-/**
- * Created by AlexZandR on 4/24/17
- */
-
-interface ItemSwipeListener {
-    fun onItemSwiped(holder: ContactHolder, swipeDir: Int)
-}
--- app/src/main/java/test/alexzander/swipetodelete/UndoClickListener.kt
+++
@@ -1,9 +0,0 @@
-package test.alexzander.swipetodelete
-
-/**
- * Created by AlexZandR on 4/24/17
- */
-
-interface UndoClickListener {
-    fun onUndoClick(position: Int)
-}
