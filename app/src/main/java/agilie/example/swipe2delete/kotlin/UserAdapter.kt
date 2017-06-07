62db2832cbf333d77e936df9480345cd2ad0881a
Roman Kapshuk <roman.kapshuk@agilie.com>
2017-06-07 13:24:43 +0300

    Change dependency

--- app/src/main/java/agilie/example/swipe2delete/kotlin/UserAdapter.kt
+++ app/src/main/java/agilie/example/swipe2delete/kotlin/UserAdapter.kt
@@ -7,8 +7,6 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.AccelerateInterpolator
-import android.view.animation.Interpolator
 import com.agilie.swipe2delete.ModelOptions
 import com.agilie.swipe2delete.SwipeToDeleteDelegate
 import com.agilie.swipe2delete.interfaces.IAnimationUpdateListener
